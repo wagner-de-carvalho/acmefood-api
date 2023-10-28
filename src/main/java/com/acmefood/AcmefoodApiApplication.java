c605491601eb150c222fc8d086475bd768a45f80
mindlab <wagncarv@outlook.com>
2023-10-28 00:51:52 -0300

    first commit

---
+++ src/main/java/com/acmefood/AcmefoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.acmefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmefoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcmefoodApiApplication.class, args);
+	}
+
+}
